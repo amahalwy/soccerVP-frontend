96f8f2b625b11c3bbbb5fd4a56d5921f3b5e046f
Mohamed El Mahallawy <mmahalwy@gmail.com>
2020-11-02 11:13:22 -0800

    reset CSS

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,23 +1,49 @@
-import '../styles/globals.css';
-import React from "react";
-import { theme } from "@chakra-ui/core";
-import { Provider } from 'react-redux'
-import { useStore } from '../redux/store'
-import { ThemeProvider } from "@chakra-ui/core";
+import React from 'react';
+import { Global, css } from '@emotion/core';
+import { theme } from '@chakra-ui/core';
+import { Provider } from 'react-redux';
+import { useStore } from '../redux/store';
+import { ThemeProvider, CSSReset } from '@chakra-ui/core';
 import Nav from '../components/nav';
 
+const GlobalStyle = () => {
+  return (
+    <Global
+      styles={css`
+        ::selection {
+          background-color: #47a3f3;
+          color: #fefefe;
+        }
+
+        html {
+          min-width: 360px;
+          scroll-behavior: smooth;
+        }
+
+        #__next {
+          display: flex;
+          flex-direction: column;
+          min-height: 100vh;
+          background: white;
+        }
+      `}
+    />
+  );
+};
+
 function MyApp({ Component, pageProps }) {
-  const store = useStore(pageProps.initialReduxState)
+  const store = useStore(pageProps.initialReduxState);
 
   return (
-  <Provider store={store}>
     <ThemeProvider theme={theme}>
-      <Nav/>
-      <Component {...pageProps} />
+      <CSSReset />
+      <GlobalStyle />
+      <Provider store={store}>
+        <Nav />
+        <Component {...pageProps} />
+      </Provider>
     </ThemeProvider>
-  </Provider>
-  )
+  );
 }
 
-
-export default MyApp
+export default MyApp;
