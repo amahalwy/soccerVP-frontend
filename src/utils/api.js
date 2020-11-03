b4d2f63c78ebf53b6cdc61fd7522bce77370b287
Mohamed El Mahallawy <mmahalwy@gmail.com>
2020-11-02 20:02:41 -0800

    jwt header

--- src/utils/api.js
+++ src/utils/api.js
@@ -1,5 +1,9 @@
 const API_HOST = process.env.NODE_ENV === 'production' ? 'production_url' : 'http://localhost:5000';
 
-const makeUrl = (path) => `${API_HOST}${path}`
+const makeUrl = (path) => `${API_HOST}${path}`;
 
-export const getEvent = (key, id) => fetch(makeUrl(`/events/${id}`)).then(r => r.json())
+export const getEvent = (key, id) => fetch(makeUrl(`/events/${id}`), {
+  headers: {
+    Authorization: '... get the localStorage JWT key ...',
+  }
+}).then(r => r.json())
